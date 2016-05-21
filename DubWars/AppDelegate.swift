98e1c423f5e37718a7d971b434165699b2712b1f
Carl Gödecken <carl.goedecken@student.hpi.de>
2016-05-21 18:03:30 +0200

    Add new logos

--- DubWars/AppDelegate.swift
+++ DubWars/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
+        UINavigationBar.appearance().setBackgroundImage(UIImage.init(named: "logo.png"), forBarMetrics: UIBarMetrics.Default)
         return true
     }
     
