7a4f05807547a74478827b6f4323f87d759ccac2
alec.schneider <alec.schneider@student.hpi.de>
2016-05-21 14:44:05 +0200

    added some new views + controllers

---
+++ DubWars/ScoreboardViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ScoreBoardViewController.swift
+//  DubWars
+//
+//  Created by Alec Schneider on 21/05/16.
+//  Copyright © 2016 Carl Julius Gödecken. All rights reserved.
+//
+
+import UIKit
+
+class ScoreboardViewController: ViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
