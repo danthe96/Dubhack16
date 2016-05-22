f83b66d24efa99e561bc210d89239edaffb31512
Nils Strelow <Nils Strelow>
2016-05-22 04:11:47 +0200

    finish voting elo system

--- pyserver/server.py
+++ pyserver/server.py
@@ -7,7 +7,7 @@ import math
 
 firebase = Firebase('https://project-***REMOVED***.firebaseio.com/')
 fb_contests = Firebase('https://project-***REMOVED***.firebaseio.com/dubwars/contests')
-#print result
+fb_votes = Firebase('https://project-***REMOVED***.firebaseio.com/dubwars/votes')
 
 URL = 'https://dubhack.dubsmash.com'
 
@@ -43,6 +43,50 @@ def get_contests():
     #print contests
     return contests
 
+def get_votes():
+    votes = fb_votes.get()
+    #print votes
+    return votes
+
+def get_contest(snip, winner, loser):
+    dubs = Firebase('https://project-***REMOVED***.firebaseio.com/dubwars/contests/{0}/dubs/'.format(snip)).get()
+    return dubs[winner], dubs[loser]
+
+
+
+def process_votes():
+    print('=== Processing votes ===')
+    contests = get_contests()
+    votes = get_votes()
+
+    if not votes:
+        print('=== No votes to process ===')
+        return
+
+    for key, vote in votes.iteritems():
+        #print vote
+        snip = vote['snipID']
+        winner, loser = get_contest(snip, vote['winner'], vote['loser'])
+        winner_elo = winner['elo']
+        loser_elo = loser['elo']
+        winner_count = winner['count']
+        loser_count = loser['count']
+        eloW, eloL = newElo(winner_elo, winner_count, loser_elo, loser_count, 0)
+        update_elo(snip, vote['winner'], eloW, winner_count+1)
+        update_elo(snip, vote['loser'], eloL, loser_count+1)
+        delete_vote(key)
+
+    print('=== Finished processing votes ===')
+
+def update_elo(snip, creator, elo, count):
+    print('=== Update Elo ===')
+    update = {'elo': elo, 'count': count}
+    result = Firebase('https://project-***REMOVED***.firebaseio.com/dubwars/contests/{0}/dubs/{1}'.format(snip, creator)).update(update)
+
+def delete_vote(key):
+    print('=== Remove vote ===')
+    result = Firebase('https://project-***REMOVED***.firebaseio.com/dubwars/votes/{0}'.format(key)).delete()
+
 def newElo(Ra, countA, Rb, countB, result):
     Ea = 1/(1 + math.pow(10, ((Rb-Ra)/400)))
     Eb = 1 - Ea
@@ -77,17 +121,20 @@ def newElo(Ra, countA, Rb, countB, result):
     return int(round(newRa)), int(round(newRb))
 
 def add_dub(video):
+    print('=== Add dub ===')
     creator = video['creator']
     snip = video['snip']
 
     entry = {
-         'elo': 1200,
+         'elo': 1000,
+         'count': 0,
          'video': video
      }
     result = Firebase('https://project-***REMOVED***.firebaseio.com/dubwars/contests/{0}/dubs/{1}'.format(snip,creator)).set(entry)
     print result
 
 def import_all_dubs():
+    print('=== Importing all dubs ===')
     dubs = get_dubs()
     contests = get_contests()
 
@@ -103,5 +150,7 @@ def import_all_dubs():
            print result
 
        add_dub(video)
+    print('=== Finished importing all dubs ===')
 
 import_all_dubs()
+process_votes()
