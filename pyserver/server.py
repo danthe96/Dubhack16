3491dd18a874b3c0273f7d4f947888a887e0b4dd
Nils Strelow <Nils Strelow>
2016-05-22 02:47:02 +0200

    importing all dubs into firebase

--- pyserver/server.py
+++ pyserver/server.py
@@ -1,11 +1,11 @@
-from firebase import firebase
+from firebase import Firebase
 import requests
 import os
 import hashlib
 import base64
 
-firebase = firebase.FirebaseApplication('https://project-***REMOVED***.firebaseio.com/', None)
-result = firebase.get('/dubwars', None)
+firebase = Firebase('https://project-***REMOVED***.firebaseio.com/')
+fb_contests = Firebase('https://project-***REMOVED***.firebaseio.com/dubwars/contests')
 #print result
 
 URL = 'https://dubhack.dubsmash.com'
@@ -14,8 +14,6 @@ CLIENT_ID = '***REMOVED***'
 CLIENT_SECRET = '***REMOVED***'
 USERNAME = '***REMOVED***'
 PASSWORD = '***REMOVED***'
-
-
 
 def get_dubs():
     login_data = {
@@ -37,5 +35,41 @@ def get_dubs():
     }
     group_uuid = '***REMOVED***'
     get_dubs = requests.get('{0}/groups/{1}/dubs'.format(URL, group_uuid), headers=headers).json()
-    print get_dubs
-    return get_dubs
+    #print get_dubs['results']
+    return get_dubs['results']
+
+def get_contests():
+    contests = fb_contests.get()
+    #print contests
+    return contests
+
+def add_dub(video):
+    creator = video['creator']
+    snip = video['snip']
+
+    entry = {
+         'elo': 1200,
+         'video': video
+     }
+    result = Firebase('https://project-***REMOVED***.firebaseio.com/dubwars/contests/{0}/dubs/{1}'.format(snip,creator)).set(entry)
+    #print result
+
+def import_all_dubs():
+    dubs = get_dubs()
+    contests = get_contests()
+
+    for dub in dubs:
+       #print 'Dub :', dub, '\n'
+       video = dub['video']
+
+       if not video['snip'] in contests:
+           print('No contest with this snip yet. Adding...')
+           creator = video['creator']
+           snip = video['snip']
+           contest = {snip: {'snipId': snip,'dubs': {}}}
+           result = Firebase('https://project-***REMOVED***.firebaseio.com/dubwars/contests/').update(contest)
+           #print result
+
+       add_dub(video)
+
+import_all_dubs()
