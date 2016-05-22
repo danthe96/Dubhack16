c10e8629f88ae8ad3ef3f231b17f63e8c499690a
alec.schneider <alec.schneider@student.hpi.de>
2016-05-22 02:06:33 +0200

    elo calculations added

--- pyserver/server.py
+++ pyserver/server.py
@@ -3,6 +3,7 @@ import requests
 import os
 import hashlib
 import base64
+import math
 
 firebase = firebase.FirebaseApplication('https://project-***REMOVED***.firebaseio.com/', None)
 result = firebase.get('/dubwars', None)
@@ -39,3 +40,36 @@ def get_dubs():
     get_dubs = requests.get('{0}/groups/{1}/dubs'.format(URL, group_uuid), headers=headers).json()
     print get_dubs
     return get_dubs
+
+def newElo(Ra, countA, Rb, countB, result):
+    Ea = 1/(1 + math.pow(10, ((Rb-Ra)/400)))
+    Eb = 1 - Ea
+
+    if result == 0:
+        Sa = 1
+        Sb = 0
+    elif result == 1:
+        Sa = 0
+        Sb = 1
+    else:
+        Sa = 0.5
+        Sb = 0.5
+
+    if countA < 30:
+        ka = 40
+    elif Ra > 2400:
+        ka = 10
+    else:
+        ka = 20
+
+    if countB < 30:
+        kb = 40
+    elif Rb > 2400:
+        kb = 10
+    else:
+        kb = 20
+
+    newRa = Ra + ka * (Sa - Ea)
+    newRb = Rb + kb * (Sb - Eb)
+
+    return int(round(newRa)), int(round(newRb))
